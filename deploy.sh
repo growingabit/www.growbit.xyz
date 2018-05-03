f2b3ffd6c3c3105b9999c08c45fb7d92e2af37c1
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2018-05-04 00:51:25 +0200

    fix deploy script: upload only folder contents and skip gsutil web set that stuck

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-gsutil -m \
+cd html5up-stellar && \
+    gsutil -m \
     -h "Cache-Control:public, max-age=3600" \
-    cp -R ./html5up-stellar gs://www.growbit.xyz
+    cp -R . gs://www.growbit.xyz/
 
 gsutil -m acl ch -r -u AllUsers:R gs://www.growbit.xyz
 
-gsutil web set -m index.html -e index.html gs://www.growbit.xyz
+# gsutil web set -m index.html -e index.html gs://www.growbit.xyz
 
 gcloud compute url-maps invalidate-cdn-cache growingabit-io --path "/*"
