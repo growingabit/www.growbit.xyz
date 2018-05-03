e1278637650ed7d6b718dc6da060312de1e33a0a
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2018-05-04 00:43:15 +0200

    using a gs bucket with a domain name

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ set -e
 
 gsutil -m \
     -h "Cache-Control:public, max-age=3600" \
-    cp -R ./html5up-stellar gs://www-growbit-xyz-backend
+    cp -R ./html5up-stellar gs://www.growbit.xyz
 
-gsutil -m acl ch -r -u AllUsers:R gs://www-growbit-xyz-backend
+gsutil -m acl ch -r -u AllUsers:R gs://www.growbit.xyz
 
-gsutil web set -m index.html -e index.html gs://www-growbit-xyz-backend
+gsutil web set -m index.html -e index.html gs://www.growbit.xyz
 
 gcloud compute url-maps invalidate-cdn-cache growingabit-io --path "/*"
